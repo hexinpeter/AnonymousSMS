4ccc76ad3964b9c06add5262dc1e2e92675a6e21
Peter He <hexinpeter@gmail.com>
2015-04-18 20:00:25 +1000

    number validation added

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ActiveRecord::Base
+	validates :to, :content, presence: true
+	validates :to, format: { with: %r{\A((\+61)|0)\d{9}\Z}ix,
+													 message: "Please provide a valid Australian number. No spaces." }
 end
