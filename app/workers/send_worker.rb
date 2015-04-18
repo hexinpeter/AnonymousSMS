2bcfc86841e9be41b76e453b4f2d6b1012d807fb
Peter He <hexinpeter@gmail.com>
2015-04-19 02:26:16 +1000

    mid now saved to db

--- app/workers/send_worker.rb
+++ app/workers/send_worker.rb
@@ -4,8 +4,8 @@ class SendWorker
 	def perform(token, id)
 		header =  {authorization: "Bearer #{token}", "Content-Type" => "application/json", "Accept" => "application/json"}
 		message = Message.find(id)
-		to = message.to
-		content = message.content
-    RestClient.post "https://api.telstra.com/v1/sms/messages", {to: to, body: content}.to_json, header
+    mid = RestClient.post "https://api.telstra.com/v1/sms/messages", {to: message.to, body: message.content}.to_json, header
+    message.mid = JSON.parse(mid)["messageId"] if mid
+    message.save!
 	end
 end
