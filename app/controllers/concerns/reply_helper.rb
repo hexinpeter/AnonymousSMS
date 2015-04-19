a0e57ce5478ffd8da7325597a03edf62def33aa2
Peter He <hexinpeter@gmail.com>
2015-04-19 16:45:58 +1000

    display all replied messages

--- app/controllers/concerns/reply_helper.rb
+++ app/controllers/concerns/reply_helper.rb
@@ -1,6 +1,7 @@
 module ReplyHelper
 	extend ActiveSupport::Concern
 
+	# return an array of Reply objects
 	def get_reply(mid)
 		if !mid
 			return
@@ -8,7 +9,11 @@ module ReplyHelper
 		token = Token.get_token
 		header =  {authorization: "Bearer #{token}", "Content-Type" => "application/json", "Accept" => "application/json"}
 		result = RestClient.get "https://api.telstra.com/v1/sms/messages/#{mid}/response", header
-		result = JSON.parse(result)[0]
-		reply = Reply.new(result['from'], result['acknowledgedTimestamp'], result['content'])
+		result = JSON.parse(result)
+		reply = []
+		result.each do |r|
+			reply.push Reply.new(r['from'], r['acknowledgedTimestamp'], r['content'])
+		end
+		reply
 	end
 end
