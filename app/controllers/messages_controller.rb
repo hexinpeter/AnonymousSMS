14b9e72b7cbe79fdb3c5ee0e8508d89fab66b4b9
Peter He <hexinpeter@gmail.com>
2015-04-19 17:00:27 +1000

    disable unused paths

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,10 +8,10 @@ class MessagesController < ApplicationController
   #   @messages = Message.all
   # end
 
-  # GET /messages/1
-  # GET /messages/1.json
-  def show
-  end
+  # # GET /messages/1
+  # # GET /messages/1.json
+  # def show
+  # end
 
   # GET /messages/new
   def new
@@ -44,29 +44,29 @@ class MessagesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /messages/1
-  # PATCH/PUT /messages/1.json
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to @message, notice: 'Message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # # PATCH/PUT /messages/1
+  # # PATCH/PUT /messages/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @message.update(message_params)
+  #       format.html { redirect_to @message, notice: 'Message was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @message }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @message.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /messages/1
-  # DELETE /messages/1.json
-  def destroy
-    @message.destroy
-    respond_to do |format|
-      format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # # DELETE /messages/1
+  # # DELETE /messages/1.json
+  # def destroy
+  #   @message.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   # GET /messages/resply/
   def reply
